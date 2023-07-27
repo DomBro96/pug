44e9337337b2f66e1aaa0a8926da8184f3b29c96
DomBro96 <460716809@qq.com>
2023-07-28 00:04:26 +0800

    feat: add nextchunk func and util floder

--- journal/journal.go
+++ journal/journal.go
@@ -1,9 +1,12 @@
 package journal
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
 
 	"github.com/dombro/pug/errors"
+	"github.com/dombro/pug/util"
 )
 
 // The wire format is that the stream is divided into 32KiB blocks, and each
@@ -24,10 +27,18 @@ const (
 )
 
 const (
-	fullChunckType = iota + 1
-	firstChunckType
-	middleChunckType
-	lastChunckType
+	checksumStart = 0
+	checksumEnd   = 4
+	lengthStart   = 4
+	lengthEnd     = 6
+	chunkType     = 6
+)
+
+const (
+	fullChunkType = iota + 1
+	firstChunkType
+	middleChunkType
+	lastChunkType
 )
 
 //  Reader reads journals from an underlying io.Reader.
@@ -66,7 +77,7 @@ func NewReader(r io.Reader, dropper Dropper, strict bool, checksum bool) *Reader
 	}
 }
 
-func (r *Reader) corrupted(n int, reason string, skip bool) error {
+func (r *Reader) corrupt(n int, reason string, skip bool) error {
 	if r.dropper != nil {
 		r.dropper.Drop(&ErrCorrupted{Size: n, Reason: reason})
 	}
@@ -77,4 +88,81 @@ func (r *Reader) corrupted(n int, reason string, skip bool) error {
 	}
 
 	return errSkip
+}
+
+// nextChunk sets r.buf[r.i:r.j] to hold the next chunk's payload, reading the
+// next block into the buffer if necessary.
+func (r *Reader) nextChunk(first bool) error {
+	for {
+		if r.j+headerSize <= r.n {
+			checksum := binary.LittleEndian.Uint32(r.buf[r.j+checksumStart : r.j+checksumEnd])
+			length := binary.LittleEndian.Uint16(r.buf[r.j+lengthStart : r.j+lengthEnd])
+			chunkType := r.buf[r.j+chunkType]
+			unprocBlock := r.n - r.j
+
+			if checksum == 0 && length == 0 && chunkType == 0 {
+				// Drop entire block.
+				r.i = r.n
+				r.j = r.n
+				return r.corrupt(unprocBlock, "zero header", false)
+			}
+
+			if chunkType < fullChunkType || chunkType > lastChunkType {
+				// Drop entire block.
+				r.i = r.n
+				r.j = r.n
+				return r.corrupt(unprocBlock, fmt.Sprintf("invalid chunk type %#x", chunkType), false)
+			}
+
+			r.i = r.j + headerSize
+			r.j = r.j + headerSize + int(length)
+
+			if r.j > r.n {
+				// Drop entire block.
+				r.i = r.n
+				r.j = r.n
+				return r.corrupt(unprocBlock, "chunk length overflows block", false)
+			}
+
+			if r.checksum && checksum != util.NewCRC(r.buf[r.i-1:r.j]).Value() {
+				// Drop entire block.
+				r.i = r.n
+				r.j = r.n
+				return r.corrupt(unprocBlock, "checksum mismatch", false)
+			}
+
+			if first && chunkType != fullChunkType && chunkType != firstChunkType {
+				chunkLength := (r.j - r.i) + headerSize
+				r.i = r.j
+				// Report the error, but skip it.
+				return r.corrupt(chunkLength, "orphan chunk", true)
+			}
+
+			r.last = chunkType == fullChunkType || chunkType == lastChunkType
+			return nil
+		}
+
+		// The last block.
+		if r.n < blockSize && r.n > 0 {
+			if !first {
+				return r.corrupt(0, "missing chunk part", false)
+			}
+			r.err = io.EOF
+			return r.err
+		}
+
+		// Read block.
+		n, err := io.ReadFull(r.r, r.buf[:])
+		if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
+			return err
+		}
+		if n == 0 {
+			if !first {
+				return r.corrupt(0, "missing chunk part", false)
+			}
+			r.err = io.EOF
+			return r.err
+		}
+		r.i, r.j, r.n = 0, 0, n
+	}
 }
